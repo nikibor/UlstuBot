3c033ec1ae925296a51746edb6236d1837963a8d
IBS\VBelov <student>
2017-09-15 08:58:44 +0400

    Прикрутил, но не уверен что работает

--- bot.py
+++ bot.py
@@ -3,9 +3,11 @@ import telebot
 import bayes
 import yandex
 import weather
+import eventsScrap
 import pymorphy2
 
 bot = telebot.TeleBot(config.token)
+
 
 # @bot.message_handler(content_types=["text"])
 # def teh_handler(message):
@@ -14,25 +16,32 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def teh_handler(message):
-    cat = bayes.classify(sentence= message.text)
+    cat = bayes.classify(sentence=message.text)
     if cat[0] == 'news':
         res = yandex.Parse_scv()
         for news in res:
             bot.send_message(message.chat.id, news)
     elif cat[0] == 'weather':
         res = weather.TakeWeather()
-        bot.send_message(message.chat.id, 'Погода за окном '+res['status'])
+        bot.send_message(message.chat.id, 'Погода за окном ' + res['status'])
         bot.send_message(message.chat.id, 'Облачность ' + res['description'])
         bot.send_message(message.chat.id, 'Восход ' + res['sunrise'])
         bot.send_message(message.chat.id, 'Закат ' + res['sunset'])
         bot.send_message(message.chat.id, 'Скорость ветра = ' + str(res['windspeed']))
     elif cat[0] == 'traffic':
         photo = yandex.TraficJam()
-        img = open('traffic.jpg','rb')
-        bot.send_photo(message.chat.id,img)
+        img = open('traffic.jpg', 'rb')
+        bot.send_photo(message.chat.id, img)
         img.close()
+    elif cat[0] == 'events':
+        events = eventsScrap.parseEventsData()
+        for item in events:
+            bot.send_message(message.chat.id, item)
     else:
         bot.send_message(message.chat.id, 'Простите, я вас не понимаю')
 
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
+
